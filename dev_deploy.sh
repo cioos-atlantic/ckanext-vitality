da977a7c85de45fbcfa314c6543d96785a61f2ab
Alexandru Ianta <aianta@dal.ca>
2021-06-23 14:03:51 -0300

    neuter the deploy script for now

--- dev_deploy.sh
+++ dev_deploy.sh
@@ -1,46 +1,59 @@
 # Dev Deployment Script
 
-# COPY PLUGIN FILES
-echo "Cloned repo into $(Pipeline.Workspace)/s/"
-echo "Repo contents:"
-ls $(Pipeline.Workspace)/s/
+# ENSURE ENVIRONMENT VARIABLES EXIST, OTHERWISE CRASH
+# IT'S CRITICAL THIS EXISTS OTHERWISE THE DEPLOYING SERVER WILL GET NUKED!!
+if [ -z "$(docker_exts_path)"] ||
+   [ -z "$(ckanexts_path)"] ||
+   [ -z "$(ext_name)"] ||
+   [ -z "$(docker_venv_path)"] ||
+   [ -z "$(docker_venv_path)"] ||
 
-# If the extension folder does not exist create it.
-if [ ! -d $(CKANEXTS_PATH)$(EXT_NAME) ]
+
 then
-    echo "Extension folder does not exist, creating it now."
-    sudo mkdir $(ckanexts_path)$(ext_name)
-else
-    echo "Extension folder exists, clearing old contents!"
-    sudo rm -r $(ckanexts_path)$(ext_name)/*
-fi
-echo "Copying new files"
-sudo cp -r $(Pipeline.Workspace)/s/* $(ckanexts_path)$(ext_name)
-sudo chmod 777 -R $(ckanexts_path)$(ext_name)
+    echo "Required environment variables not set! Exiting to prevent catastrophic behavior!"
+    exit 1
 
-echo "Installing plugin into CKAN venv"
-sudo docker exec ckan /bin/bash -c "source $(docker_venv_path) && \
-    cd $(docker_exts_path)ckanext-vitality_prototype/ && pip install -r requirements.txt"
-sudo docker exec ckan /bin/bash -c "source $(docker_venv_path) && \
-    cd $(docker_exts_path)ckanext-vitality_prototype/ && python setup.py develop"
+# # COPY PLUGIN FILES
+# echo "Cloned repo into $(Pipeline.Workspace)/s/"
+# echo "Repo contents:"
+# ls $(Pipeline.Workspace)/s/
 
-# SHUTDOWN CKAN
-cd $(docker_path)
-sudo docker-compose down 
+# # If the extension folder does not exist create it.
+# if [ ! -d $(CKANEXTS_PATH)$(EXT_NAME) ]
+# then
+#     echo "Extension folder does not exist, creating it now."
+#     sudo mkdir $(ckanexts_path)$(ext_name)
+# else
+#     echo "Extension folder exists, clearing old contents!"
+#     sudo rm -r $(ckanexts_path)$(ext_name)/*
+# fi
+# echo "Copying new files"
+# sudo cp -r $(Pipeline.Workspace)/s/* $(ckanexts_path)$(ext_name)
+# sudo chmod 777 -R $(ckanexts_path)$(ext_name)
 
-# RESTART CKAN
-cd $(docker_path)
-sudo docker-compose up -d 
+# echo "Installing plugin into CKAN venv"
+# sudo docker exec ckan /bin/bash -c "source $(docker_venv_path) && \
+#     cd $(docker_exts_path)ckanext-vitality_prototype/ && pip install -r requirements.txt"
+# sudo docker exec ckan /bin/bash -c "source $(docker_venv_path) && \
+#     cd $(docker_exts_path)ckanext-vitality_prototype/ && python setup.py develop"
 
-# SEED VITALITY AUTHORIZATION MODEL
-echo "Seeding users into metadata authorization model"
-sudo docker exec ckan /usr/local/bin/ckan-paster --plugin=ckanext-vitality_prototype \
-    vitality seed --config=/etc/ckan/production.ini
+# # SHUTDOWN CKAN
+# cd $(docker_path)
+# sudo docker-compose down 
 
-# RE-INDEX DATASETS
-echo "Re-indexing datasets..."
-sudo docker exec ckan /usr/local/bin/ckan-paster --plugin=ckan search-index \
-    rebuild --config=/etc/ckan/production.ini
+# # RESTART CKAN
+# cd $(docker_path)
+# sudo docker-compose up -d 
 
-sudo docker exec ckan /usr/local/bin/ckan-paster --plugin=ckanext-harvest harvester \
-    reindex --config=/etc/ckan/production.ini
+# # SEED VITALITY AUTHORIZATION MODEL
+# echo "Seeding users into metadata authorization model"
+# sudo docker exec ckan /usr/local/bin/ckan-paster --plugin=ckanext-vitality_prototype \
+#     vitality seed --config=/etc/ckan/production.ini
+
+# # RE-INDEX DATASETS
+# echo "Re-indexing datasets..."
+# sudo docker exec ckan /usr/local/bin/ckan-paster --plugin=ckan search-index \
+#     rebuild --config=/etc/ckan/production.ini
+
+# sudo docker exec ckan /usr/local/bin/ckan-paster --plugin=ckanext-harvest harvester \
+#     reindex --config=/etc/ckan/production.ini
