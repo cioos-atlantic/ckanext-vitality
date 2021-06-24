bc61a58ab5e5889cf71e7d85de6af8174015670d
Alexandru Ianta <aianta@dal.ca>
2021-06-24 13:20:41 -0300

    using predefined variables

--- dev_deploy.sh
+++ dev_deploy.sh
@@ -26,9 +26,9 @@ echo "DOCKER_PATH: ${DOCKER_PATH}"
 echo "DOCKER_VENV_PATH: ${DOCKER_VENV_PATH}"
 
 # COPY PLUGIN FILES
-echo "Cloned repo into $(Pipeline.Workspace)/s/"
+echo "Cloned repo into $PIPELINE_WORKSPACE/s/"
 echo "Repo contents:"
-ls $(Pipeline.Workspace)/s/
+ls $PIPELINE_WORKSPACE/s/
 
 # If the extension folder does not exist create it.
 if [ ! -d ${CKANEXTS_PATH}${EXT_NAME} ]
