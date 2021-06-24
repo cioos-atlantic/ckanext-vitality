84c84d470bb1d3996ba0516450199d6d9717241a
Alexandru Ianta <aianta@dal.ca>
2021-06-24 13:17:41 -0300

    re-added the copy code

--- dev_deploy.sh
+++ dev_deploy.sh
@@ -25,23 +25,23 @@ echo "EXT_NAME: ${EXT_NAME}"
 echo "DOCKER_PATH: ${DOCKER_PATH}"
 echo "DOCKER_VENV_PATH: ${DOCKER_VENV_PATH}"
 
-# # COPY PLUGIN FILES
-# echo "Cloned repo into $(Pipeline.Workspace)/s/"
-# echo "Repo contents:"
-# ls $(Pipeline.Workspace)/s/
+# COPY PLUGIN FILES
+echo "Cloned repo into $(Pipeline.Workspace)/s/"
+echo "Repo contents:"
+ls $(Pipeline.Workspace)/s/
 
-# # If the extension folder does not exist create it.
-# if [ ! -d $(CKANEXTS_PATH)$(EXT_NAME) ]
-# then
-#     echo "Extension folder does not exist, creating it now."
-#     sudo mkdir $(ckanexts_path)$(ext_name)
-# else
-#     echo "Extension folder exists, clearing old contents!"
-#     sudo rm -r $(ckanexts_path)$(ext_name)/*
-# fi
-# echo "Copying new files"
-# sudo cp -r $(Pipeline.Workspace)/s/* $(ckanexts_path)$(ext_name)
-# sudo chmod 777 -R $(ckanexts_path)$(ext_name)
+# If the extension folder does not exist create it.
+if [ ! -d ${CKANEXTS_PATH}${EXT_NAME} ]
+then
+    echo "Extension folder does not exist, creating it now."
+    sudo mkdir ${CKANEXTS_PATH}${EXT_NAME}
+else
+    echo "Extension folder exists, clearing old contents in ${CKANEXTS_PATH}${EXT_NAME}"
+    # sudo rm -r ${CKANEXTS_PATH}${EXT_NAME}
+fi
+echo "Copying new files"
+sudo cp -r $(Pipeline.Workspace)/s/* ${CKANEXTS_PATH}${EXT_NAME}
+sudo chmod 777 -R ${CKANEXTS_PATH}${EXT_NAME}
 
 # echo "Installing plugin into CKAN venv"
 # sudo docker exec ckan /bin/bash -c "source $(docker_venv_path) && \
